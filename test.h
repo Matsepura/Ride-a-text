289d20a70e0f0e9018aaf7f3c2ff2c89a361be6a
Semen Matsepura <a3:ac:3e:63:22:72:39:39:7f:83:fc:f2:77:42:fc:a9>
2015-09-15 12:30:50 +0300

    commit before rename

---
+++ test.h
@@ -0,0 +1,13 @@
+//
+//  test.h
+//  Ride a text
+//
+//  Created by Semen on 15.09.15.
+//  Copyright (c) 2015 Semen Matsepura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface test : UIViewController
+
+@end
---
+++ test.m
@@ -0,0 +1,13 @@
+//
+//  test.m
+//  Ride a text
+//
+//  Created by Semen on 15.09.15.
+//  Copyright (c) 2015 Semen Matsepura. All rights reserved.
+//
+
+#import "test.h"
+
+@implementation test
+
+@end
